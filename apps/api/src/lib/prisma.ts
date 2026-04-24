68c9feccd9cf272c556949f65267094a7c0221b6
Lucas Guerra <lucasguerrawr2004@gmail.com>
2026-04-24 11:29:13 -0300

    deleting api and restarting

--- apps/api/src/lib/prisma.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-import { env } from '../env';
-
-// Mostra as queries SQL no terminal apenas se estivermos em desenvolvimento
-export const prisma = new PrismaClient({
-  log: env.NODE_ENV === 'dev' ? ['query'] : [],
-});
